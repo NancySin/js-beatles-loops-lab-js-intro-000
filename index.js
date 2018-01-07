7bc4c78716298a80861b38e3adbed6515c6b84ef
nancy sin <nsin@sbcglobal.net>
2018-01-07 19:22:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function theBeatlesPlay (musicians, instruments){
-  var arry =[]
-  for (var i + 0; i< musicuan.length; i++){
+  var array =[]
+  for (var i = 0; i< musicuan.length; i++){
     var m= musicians[i];
     var instru = instruments[i]
-    array.push( `${m} plays ${instru}`)
+    array.push(`${m} plays ${instru}`)
   }
   return array;
 }
